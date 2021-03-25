01f10470964c9704361a171d549f596eafaff1ef
Anders <palantus@gmail.com>
2021-03-25 12:44:37 +0100

    add static instance, to prevent init multiple times

--- main.js
+++ main.js
@@ -3,15 +3,21 @@
 const fs = require("fs")
 const path = require("path")
 const jison = require("jison")
+let instance = null
 
 class SearchQueryParser{
   async init(){
     let bnf = await new Promise((r) => fs.readFile(path.join(__dirname, "searchtokens.jison"), "utf8", (err, data) => r(data)));
     this.parser = new jison.Parser(bnf);
+    instance = this;
   }
 
   parse(exp){
     return this.parser.parse(exp)
+  }
+
+  static instance(){
+    return instance;
   }
 }
 
