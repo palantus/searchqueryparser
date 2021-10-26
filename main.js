96abfe1f4ceb677c22a6dc77258abbccfd42ce76
Anders <palantus@gmail.com>
2021-10-26 10:23:49 +0200

    Fix race conditions

--- main.js
+++ main.js
@@ -6,14 +6,21 @@ const jison = require("jison")
 let instance = null
 
 class SearchQueryParser{
+  initPromise = null;
+
   async init(){
     if(instance){
+      await instance.initPromise
       this.parser = instance.parser
-    } else {
+    }
+
+    instance = this;
+    this.initPromise = new Promise(async resolveInit => {
       let bnf = await new Promise((r) => fs.readFile(path.join(__dirname, "searchtokens.jison"), "utf8", (err, data) => r(data)));
       this.parser = new jison.Parser(bnf);
-    }
-    instance = this;
+      resolveInit()
+    })
+    await this.initPromise
   }
 
   parse(exp){
